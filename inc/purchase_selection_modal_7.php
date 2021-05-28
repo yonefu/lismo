e0e89397d5fa17371d56e341e0861112b787be35
umesh.shakya <umesh@miracleinterface.com>
2021-05-28 15:51:44 +0545

    [ADDED] :: missing modals

---
+++ inc/purchase_selection_modal_7.php
@@ -0,0 +1,91 @@
+<div class="inner-page-wrap">
+	<div class="purchase-page w-100">
+		<div class="d-flex flex-wrap justify-content-between align-items-center mb-3">
+			<h3 class="page-title mb-1">
+				<span>&nbsp;</span>
+			</h3>
+
+			<div class="search-box">
+				<form class="form-inline">
+					<div class="input-group align-items-center">
+						<input type="text" class="form-control" placeholder="">
+						<button type="search" class="btn-search">検索</button>
+						<span class="clearable"><img src="dist/img/icons/icon-close.png" alt="" /></span>
+					</div>
+				</form>
+			</div>
+			<!--search box-->
+		</div>
+		<!--div-->
+
+		<div class="d-flex flex-wrap justify-content-between align-items-center mb-3">
+			<div class="right-dropdown flex-wrap d-flex ml-auto">
+				<div class="mb-2">
+					<a class="nav-link dropdown-warehouse btn btn-sm dropdown-toggle bg-white br-25 mr-2" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						タグ選択：なし
+					</a>
+					<div class="dropdown-menu dropdown-menu-right" style="max-width: 480px;">
+						<div class="d-flex flex-wrap registerd-tags p-4">
+							<div class="tag btn btn-outline-dark br-10 mr-2 px-3 mb-2">
+								<span class="mr-2">iPhone</span>
+							</div>
+							<div class="tag btn btn-outline-dark br-10 mr-2 px-3 mb-2">
+								<span class="mr-2">AppleJpan</span>
+							</div>
+							<div class="tag btn btn-outline-dark br-10 mr-2 px-3 mb-2">
+								<span class="mr-2">imac</span>
+							</div>
+							<div class="tag btn btn-outline-dark br-10 mr-2 px-3 mb-2">
+								<span class="mr-2">iPhone</span>
+							</div>
+							<div class="tag btn btn-outline-dark br-10 mr-2 px-3 mb-2">
+								<span class="mr-2">AppleJpan</span>
+							</div>
+							<div class="tag btn btn-outline-dark br-10 mr-2 px-3 mb-2">
+								<span class="mr-2">imac</span>
+							</div>
+							<div class="tag btn btn-outline-dark br-10 mr-2 px-3 mb-2">
+								<span class="mr-2">iPhone</span>
+							</div>
+							<div class="tag btn btn-outline-dark br-10 mr-2 px-3 mb-2">
+								<span class="mr-2">imac</span>
+							</div>
+							<div class="tag btn btn-outline-dark br-10 mr-2 px-3 mb-2">
+								<span class="mr-2">iPhone</span>
+							</div>
+							<div class="tag btn btn-outline-dark br-10 mr-2 px-3 mb-2">
+								<span class="mr-2">AppleJpan</span>
+							</div>
+							<div class="tag btn btn-outline-dark br-10 mr-2 px-3 mb-2">
+								<span class="mr-2">imac</span>
+							</div>
+							<div class="tag btn btn-outline-dark br-10 mr-2 px-3 mb-2">
+								<span class="mr-2">iPhone</span>
+							</div>
+							<div class="tag btn btn-outline-dark br-10 mr-2 px-3 mb-2">
+								<span class="mr-2">AppleJpan</span>
+							</div>
+							<div class="tag btn btn-outline-dark br-10 mr-2 px-3 mb-2">
+								<span class="mr-2">imac</span>
+							</div>
+							<div class="tag btn btn-outline-dark br-10 mr-2 px-3 mb-2">
+								<span class="mr-2">iPhone</span>
+							</div>
+							<div class="tag btn btn-outline-dark br-10 mr-2 px-3 mb-2">
+								<span class="mr-2">imac</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="bg-white br-25">
+			<p style="min-height: 300px;"></p>
+		</div>
+
+	</div>
+	<!--purchase-page w-100-->
+</div>
+<!--inner-page-wrap-->
+
---
+++ inc/purchase_selection_modal_8.php
@@ -0,0 +1,42 @@
+<div class="inner-page-wrap">
+	<div class="purchase-page w-100">
+		<div class="d-flex flex-wrap justify-content-between align-items-center mb-3">
+			<h3 class="page-title mb-1">
+				<span>&nbsp;</span>
+			</h3>
+
+			<div class="search-box">
+				<form class="form-inline">
+					<div class="input-group align-items-center">
+						<input type="text" class="form-control" placeholder="">
+						<button type="search" class="btn-search">検索</button>
+						<span class="clearable"><img src="dist/img/icons/icon-close.png" alt="" /></span>
+					</div>
+				</form>
+			</div>
+			<!--search box-->
+		</div>
+		<!--div-->
+
+		<div class="d-flex flex-wrap justify-content-between align-items-center mb-3">
+			<div class="right-dropdown flex-wrap d-flex ml-auto">
+				<div class="mb-2">
+					<a class="nav-link dropdown-warehouse btn btn-sm dropdown-toggle bg-white br-25 mr-2" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						期間：今⽉
+					</a>
+					<div class="dropdown-menu dropdown-menu-right" style="max-width: 480px;">
+						<img src="dist/img/calendar.png" alt="" />
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="bg-white br-25">
+			<p style="min-height: 300px;"></p>
+		</div>
+
+	</div>
+	<!--purchase-page w-100-->
+</div>
+<!--inner-page-wrap-->
+
